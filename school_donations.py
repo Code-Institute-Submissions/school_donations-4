9687145e92563115f590a9ee1018c9a4ddb28709
Philneeves <p.neeves@gmail.com>
2017-04-19 16:02:21 +0100

    explanatory commenting for python file added

--- school_donations.py
+++ school_donations.py
@@ -8,22 +8,30 @@ import os
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'ds033259.mlab.com'  # This time we won't be using the digits after the colon - eg. ds047305.mlab.com
-MONGODB_PORT = 33259  # This is for the port. This where the digits after the colon go.
-DBS_NAME = 'heroku_fqw52m2t'  # Where MONGO_DB_NAME is your database name - eg. heroku_1wgm307k
+MONGODB_HOST = 'ds033259.mlab.com'
+MONGODB_PORT = 33259
+DBS_NAME = 'heroku_fqw52m2t'
 MONGO_URI = os.getenv('MONGODB_URI')
 COLLECTION_NAME = 'projects'
 FIELDS = {'primary_focus_area': True, 'funding_status': True, 'school_state': True, 'resource_type': True,
-          'poverty_level': True, 'date_posted': True, 'total_donations': True, '_id': False}
+          'poverty_level': True, 'date_posted': True, 'total_donations': True, '_id': False} 
+# FIELDS is the constant that defines the record fields that we wish to retrieve and form the basis of the project
 
 
 @app.route("/")
 def index():
+    """
+    A Flask view to serve the main dashboard page.
+    """
     return render_template("index.html")
 
 
 @app.route("/donorsUS/projects")
 def donor_projects():
+    """
+    A Flask view to serve the project data from
+    MongoDB in JSON format.
+    """
     connection = MongoClient(MONGO_URI)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     projects = collection.find(projection=FIELDS, limit=20000)
